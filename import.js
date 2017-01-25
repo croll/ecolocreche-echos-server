65343eef7ad80f742d59369fed0d4438d6ba4ca5
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-01-25 13:32:06 +0100

    import of establishment

--- import.js
+++ import.js
@@ -9,32 +9,77 @@ var models_import = require(__dirname + '/models_import');
 models.sequelize.sync({
     force: true,
 }).then(function() {
-    models_import.users.findAll().then(function(users) {
-        var p = new Promise(function (resolve, reject) {
+    var p = new Promise(function (resolve, reject) {
+        resolve();
+    });
+
+    // import des users
+    p = p.then(function() {
+        return models_import.users.findAll();
+    });
+    p = p.then(function(rows) {
+        var p2 = new Promise(function (resolve, reject) {
             resolve();
         });
-        users.forEach(function(user) {
-            p=p.then(function() {
+        rows.forEach(function(row) {
+            p2=p2.then(function() {
                 return models.users.create({
-                    name: user.user_name,
-                    password_hash: user.user_password_hash,
-                    email: user.user_email,
-                    account_type: user.user_account_type ? 'agent' : 'admin',
-                    rememberme_token: user.user_rememberme_token ? user.user_rememberme_token : '',
-                    creation_timestamp: user.user_creation_timestamp ? user.user_creation_timestamp : "1975-02-06",
+                    name: row.user_name,
+                    password_hash: row.user_password_hash,
+                    email: row.user_email,
+                    account_type: row.user_account_type ? 'agent' : 'admin',
+                    rememberme_token: row.user_rememberme_token ? row.user_rememberme_token : '',
+                    creation_timestamp: row.user_creation_timestamp ? row.user_creation_timestamp : "1975-02-06",
                 }).then(function() {
-                    console.log("user "+user.user_name+" imported.");
+                    console.log("user "+row.user_name+" imported.");
                 }, function(err) {
-                    console.log("user "+user.user_name+" error: ", err);
+                    console.log("user "+row.user_name+" error: ", err);
                 });
             });
         });
-        return p;
+        return p2;
     }).then(function() {
         console.log("users imported ok");
     }, function(err) {
         console.error("can't import users: ", err);
     });
+
+
+    // import des etablissements
+    p = p.then(function() {
+        return models_import.etablissement.findAll();
+    });
+    p = p.then(function(rows) {
+        var p2 = new Promise(function (resolve, reject) {
+            resolve();
+        });
+        rows.forEach(function(row) {
+            p2=p2.then(function() {
+                return models.establishment.create({
+                    name: row.nom,
+                    address: row.address ? row.address : '',
+                    postalcode: row.codepostal ? row.codepostal : '',
+                    city: row.ville ? row.ville : '',
+                    phone: row.telephone ? row.telephone : '',
+                    mail: row.mail ? row.mail : '',
+                    type: ['creche', 'halte-garderie', 'micro-creche', 'multi-accueil', 'relais-d-assistante', 'autre'][row.type],
+                    status: ['association','association-parentale','entreprise','publique','indetermine','autre'][row.statut],
+                }).then(function() {
+                    console.log("establishment "+row.nom+" imported.");
+                }, function(err) {
+                    console.log("establishment "+row.nom+" error: ", err);
+                });
+            });
+        });
+        return p2;
+    }).then(function() {
+        console.log("establishment imported ok");
+    }, function(err) {
+        console.error("can't import establishment: ", err);
+    });
+
+    return p;
+
 }, function(err) {
     console.error("can't sync db ", err);
 });
