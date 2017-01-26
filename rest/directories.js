e6539b3938385983afbcb4350c04f594c139fa38
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-01-26 17:45:38 +0100

    add rest /rest/directories/roots

--- rest/directories.js
+++ rest/directories.js
@@ -1,11 +1,33 @@
 module.exports = function(server, epilogue, models) {
 
+
+    /*
+     * list any directories
+     */
     var nodeResource = epilogue.resource({
         endpoints: ['/rest/directories', '/rest/directories/:id'],
-        model: models.node,
+        model: models.node.scope,
         include: [{
             model: models.node_hist
-        }]
+        }],
+    });
+
+
+    /*
+     * list root directories
+     */
+    models.node.addScope('roots', {
+        where: {
+            "id_directory_parent": null,
+        }
+    });
+
+    var nodeResource = epilogue.resource({
+        endpoints: ['/rest/directories/roots', '/rest/directories/roots/:id'],
+        model: models.node.scope('roots'),
+        include: [{
+            model: models.node_hist
+        }],
     });
 
 }
