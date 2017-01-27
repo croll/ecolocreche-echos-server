e48c42b97b0f2b0c8166ea49b145d41775c933fe
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-01-27 10:53:37 +0100

    fix /rest/hist/directories/

--- rest/directories.js
+++ rest/directories.js
@@ -57,7 +57,7 @@ module.exports = function(server, epilogue, models) {
              where nh1.id_node = nh2.id_node
              and createdAt <= ?
             )
-            and deletedAt IS NOT NULL`;
+            and deletedAt IS NULL`;
 
         var replacements = [ date ];
 
