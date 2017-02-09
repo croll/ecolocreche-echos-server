18be527ebf4402f12b5f4549bd47290c68defac8
Nicolas Dimitrijevic <niclone@croll.fr>
2017-02-09 18:11:45 +0100

    allow agent to view/list establishments

--- rest/establishments.js
+++ rest/establishments.js
@@ -4,6 +4,13 @@ module.exports = function(server, epilogue, models, permchecks) {
       model: models.establishment,
       endpoints: ['/rest/establishments', '/rest/establishments/:id']
     });
-    establishmentResource.use(permchecks.default_permissions);
+    //establishmentResource.use(permchecks.default_permissions);
+
+    // if we don't use the default permissions above, we must set it for all possible rest call (create, list, read, update and delete)
+    establishmentResource.create.auth.before(permchecks.haveAdmin);
+    establishmentResource.list.auth.before(permchecks.haveAgent);
+    establishmentResource.read.auth.before(permchecks.haveAgent);
+    establishmentResource.update.auth.before(permchecks.haveAdmin);
+    establishmentResource.delete.auth.before(permchecks.haveAdmin);
 
 }
