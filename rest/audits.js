38f1e39fe43ae4af8894fde856ea237a27186794
Nicolas Dimitrijevic <niclone@croll.fr>
2017-02-09 17:23:59 +0100

    allow agent/super agent to deal with audits

--- rest/audits.js
+++ rest/audits.js
@@ -4,7 +4,14 @@ module.exports = function(server, epilogue, models, permchecks) {
       model: models.audit,
       endpoints: ['/rest/audits', '/rest/audits/:id']
     });
-    auditResource.use(permchecks.default_permissions);
 
+    //auditResource.use(permchecks.default_permissions);
+
+    // if we don't use the default permissions above, we must set it for all possible rest call (create, list, read, update and delete)
+    auditResource.create.auth.before(permchecks.haveSuperAgent);
+    auditResource.list.auth.before(permchecks.haveAgent);
+    auditResource.read.auth.before(permchecks.haveAgent);
+    auditResource.update.auth.before(permchecks.haveAdmin);
+    auditResource.delete.auth.before(permchecks.haveAdmin);
 
 }
