c6be775d41e34eba6faa1addbc264d16feb6d92c
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-03-07 14:56:11 +0100

    rest/pdf working

--- rest/wkhtmltopdf.js
+++ rest/wkhtmltopdf.js
@@ -3,25 +3,22 @@ var fs = require('fs');
 
 module.exports = function(server, epilogue, models, permchecks) {
 
- //     var doc = new jsPDF();
-//    var specialElementHandlers = {
-  //              '#bypassme': function (element, renderer) {
-   //                 return true;
-    //            }
-     //       };
-    //        doc.fromHTML(source, 0.5, 0.5, {
-    //            'width': 75,'elementHandlers': specialElementHandlers
-     //       });
+    server.post('/rest/pdf',
+      permchecks.haveAgent,
+      function (req, res, next) {
+        try {
+            var stream = wkhtmltopdf(req.body, {
+                //javascriptDelay: 10000,
+                //enableJavascript: true,
+                //noStopSlowScripts: true,
+            });
 
-
-    // server.post('/rest/pdf',
-    //     //permchecks.haveAgent,
-    //     function (req, res, next) {
-    //       // wkhtmltopdf(req.body).pipe(res);
-    //       console.log(req.body);
-    //       // wkhtmltopdf(req.body, {output: 'out.pdf'});
-    //       wkhtmltopdf('http://localhost:1242/audit/205/rapport', {javascriptDelay: 10000, enableJavascript: true, noStopSlowScripts: true}).pipe(fs.createWriteStream('out.pdf'));
-    //     }
-    // );
+            res.setHeader('content-type', 'application/pdf');
+            stream.pipe(res);
+        } catch (err) {
+            console.error(err);
+        }
+      }
+    );
 
 }
