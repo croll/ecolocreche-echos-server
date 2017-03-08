f93ba0809b03aa516bcf56bcb390619ddcc0dd26
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-03-08 10:57:28 +0100

    add margins

--- rest/wkhtmltopdf.js
+++ rest/wkhtmltopdf.js
@@ -7,7 +7,20 @@ module.exports = function(server, epilogue, models, permchecks) {
       permchecks.haveAgent,
       function (req, res, next) {
         try {
+            // for debug, save html file in /tmp
+            fs.writeFile("/tmp/test.html", req.body, function(err) {
+                if(err) {
+                    return console.log(err);
+                } else {
+                    console.log("The file was saved!");
+                }
+            });
+
             var stream = wkhtmltopdf(req.body, {
+                marginTop: 20,
+                marginBottom: 20,
+                marginLeft: 20,
+                marginRight: 20,
                 //javascriptDelay: 10000,
                 //enableJavascript: true,
                 //noStopSlowScripts: true,
