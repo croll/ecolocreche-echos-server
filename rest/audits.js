182dcad10edec643a921299e21a9a537dc3015d6
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-02-06 14:00:27 +0100

    fix recap action url in mail

--- rest/audits.js
+++ rest/audits.js
@@ -85,7 +85,7 @@ module.exports = function(server, epilogue, models, permchecks) {
                     establishment_mail: audit.establishment.get('mail'),
                     audit_url: config.httpd.url+`/audit/`+audit.get('key'),
                     audit_synthesis: audit.synthesis,
-                    recapaction_url: config.httpd.url+`/audit/`+audit.get('key'),
+                    recapaction_url: config.httpd.url+`/audit/`+audit.get('id'),
                     recapaction_synthesis: audit.synthesis,
                     inquiryform_title: inquiryform_hist.title,
                     inquiryform_description: inquiryform_hist.description,
