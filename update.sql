4267222a5b6c2f3234b166f2ae3cde96f57a48dc
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-02-16 18:25:31 +0100

    ajout de ram

--- update.sql
+++ update.sql
@@ -4,7 +4,7 @@
 --
 
 ALTER TABLE `establishment` CHANGE `type` `type` ENUM('creche','halte-garderie','micro-creche','multi-accueil','relais-d-assistante','autre','assistant-maternel') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'autre';
-ALTER TABLE `establishment` CHANGE `status` `status` ENUM('association','association-parentale','entreprise','publique','indetermine','autre','domicile','creche-familiale','mam') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'autre';
+ALTER TABLE `establishment` CHANGE `status` `status` ENUM('association','association-parentale','entreprise','publique','indetermine','autre','domicile','creche-familiale','ram','mam') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'autre';
 
 
 --
